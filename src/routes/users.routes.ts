c68fe2478b3a8c32f6618018678273d32dbbff11
ErickMarchetti <erickgmarchetti@gmail.com>
2023-01-04 09:39:21 -0300

    feat/ rota de get por id do usuario

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
 import deleteUserController from "../controllers/users/deleteUser.controller";
+import readUserController from "../controllers/users/readUser.controller";
 import idOwnerVerifierMiddleware from "../middlewares/idOwnerVerifier.middleware";
 import verifyTokenMiddleware from "../middlewares/verifyToken.middleware";
 
-
 const usersRouter = Router();
 
-usersRouter.delete("/:id",verifyTokenMiddleware,idOwnerVerifierMiddleware,deleteUserController)
+usersRouter.get(
+    "/:id",
+    verifyTokenMiddleware,
+    idOwnerVerifierMiddleware,
+    readUserController
+);
+
+usersRouter.delete(
+    "/:id",
+    verifyTokenMiddleware,
+    idOwnerVerifierMiddleware,
+    deleteUserController
+);
+
+export default usersRouter;
