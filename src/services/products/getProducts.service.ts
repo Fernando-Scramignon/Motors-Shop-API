b891010fd2f19429fa902985dcb6a062b7f935a9
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-01-04 17:12:48 -0300

    fix/retorna usuarios com produtos

--- src/services/products/getProducts.service.ts
+++ src/services/products/getProducts.service.ts
@@ -3,7 +3,7 @@ import { Product } from "../../entities/products.entity";
 
 async function getProductsService(id: string) {
     const productRepository = AppDataSource.getRepository(Product);
-    const products = productRepository.find();
+    const products = productRepository.find({ relations: { user: true } });
     return products;
 }
 
